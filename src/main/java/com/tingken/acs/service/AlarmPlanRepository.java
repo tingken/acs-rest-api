2661001a84bb804889a6324f2ed8794e16ddced5
hanqiankun <hanqiankun@gmail.com>
2019-06-12 16:07:14 +0800

    add search API for alarmPlan

--- src/main/java/com/tingken/acs/service/AlarmPlanRepository.java
+++ src/main/java/com/tingken/acs/service/AlarmPlanRepository.java
@@ -4,7 +4,11 @@
 package com.tingken.acs.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -14,4 +18,9 @@ import com.tingken.acs.domain.AlarmPlan;
 @RepositoryRestResource(collectionResourceRel = "alarm_plan", path = "alarmPlans")
 public interface AlarmPlanRepository extends PagingAndSortingRepository<AlarmPlan, Long> {
     List<AlarmPlan> findByName(@Param("name") String name);
+
+    @Query("select p from AlarmPlan p where p.threshold = null")
+    Page<AlarmPlan> findAllOtherType(Pageable pageable);
+
+    Optional<AlarmPlan> findByNameAndThreshold(@Param("name") String name, @Param("threshold") String threshold);
 }
